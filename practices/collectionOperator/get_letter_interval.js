b971b3a20427d181d0d17cceb011173e14fd6333
PersistXL <pointinglxl@gmail.com>
2018-04-08 14:46:58 +0800

    '第六题'

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,22 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  var result = [];
+  var letter = ["", "a","b","c","d","e","f","g"];
+  if(number_a < number_b){
+    for(var i=number_a; i<=number_b; i++){
+      result.push(letter[i])
+    }
+  }
+  if(number_a > number_b){
+    for(var i=number_a;i>=number_b;i--){
+      result.push(letter[i]);
+    }
+  }
+  if(number_a == number_b){
+    result.push(letter[number_b]);
+  }
+    return result;
 }
 
 module.exports = get_letter_interval;
