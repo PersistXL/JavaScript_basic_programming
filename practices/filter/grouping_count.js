325853289abbb2d51d97b57dccb05bc442771a9e
PersistXL <pointinglxl@gmail.com>
2018-04-08 19:36:22 +0800

    '第四题'

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,15 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+  var result = [];
+  for(var index in collection){
+    if(result[collection[index]]){
+      result[collection[index]] += 1;
+    }else {
+      result[collection[index]] = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = grouping_count;
