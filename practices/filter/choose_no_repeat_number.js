9957d6e1cebd6adef87b11be59683d83d7dabe3e
PersistXL <pointinglxl@gmail.com>
2018-04-08 18:26:51 +0800

    '第三题'

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -2,7 +2,10 @@
 
 function choose_no_repeat_number(collection) {
 
-  //在这里写入代码
+  // 在这里写入代码
+  var result = [];
+  result = Array.from(new Set(collection));
+  return result;
 }
 
 module.exports = choose_no_repeat_number;
