e86b4b285e438025f378ed8e53a7a66e5502db21
PersistXL <pointinglxl@gmail.com>
2018-04-17 10:20:52 +0800

    '在map中数字映射字母'

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,11 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  // var result = []
+  // for(var i = 0;i<collection.length;i++){
+  //   result.push(String.fromCharCode(i+97))
+  // }
+  // return  result
+  return collection.map(result =>String.fromCharCode(result+96))
 };
 
 module.exports = number_map_to_word;
