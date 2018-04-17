8f8edc78b90c94e43024a7df7891ca4a98161a4f
PersistXL <pointinglxl@gmail.com>
2018-04-17 10:29:50 +0800

    '字母映射表二（查看别人的，自己不太理解）'

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,17 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  var letter = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var resultArray = [];
+
+  for (var index in collection) {
+    if (collection[index] % 26 === 0) {
+      resultArray.push(letter[Math.floor(collection[index] / 26) - 1].concat(letter[26]));
+    } else {
+      resultArray.push(letter[Math.floor(collection[index] / 26)].concat(letter[collection[index] % 26]));
+    }
+  }
+
+  return resultArray;
 };
 
 module.exports = number_map_to_word_over_26;
