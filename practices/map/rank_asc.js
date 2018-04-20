ceb744b57353beef2dc822b7a23348d6408c56f3
PersistXL <pointinglxl@gmail.com>
2018-04-20 15:31:18 +0800

    '完成map的所有题'

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,8 @@
 'use strict';
 var rank_asc = function(collection){
-  return [6,5,4,3,2];
+  return collection.sort(paixu)
 };
-
+function paixu(a, b) {
+  return a - b < 0;
+}
 module.exports = rank_asc;
