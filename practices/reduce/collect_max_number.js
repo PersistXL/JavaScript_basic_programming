587e81854db10dbbf862e1e88f88031751be7603
PersistXL <pointinglxl@gmail.com>
2018-04-21 19:30:17 +0800

    '选出给定数字集合元素的最大值'

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function collect_max_number(collection) {
+
   //在这里写入代码
+  for (var i = 0;i<collection.length;i++) {
+    var max = collection[0];
+    if (collection[i] > max) {
+      max = collection[i];
+    }
+  }
+  return max;
 }
 
 module.exports = collect_max_number;
