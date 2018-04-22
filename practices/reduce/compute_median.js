eadbe456cb91c39ffb1bf1268bc3f76f1f1209da
PersistXL <pointing@gmail.com>
2018-04-22 16:16:44 +0800

    求给定集合的中位数

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,6 +2,10 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  collection.sort((a, b) => a - b);
+  let low = Math.floor((collection.length - 1) / 2);
+  let higth = Math.ceil((collection.length - 1) / 2);
+  return ((collection[low] + collection[higth]) / 2);
 }
 
 module.exports = compute_median;
