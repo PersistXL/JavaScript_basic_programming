f7f17d649226435283e97d0421bbf04ded2893a1
PersistXL <pointing@gmail.com>
2018-04-22 15:41:14 +0800

    计算给定数字集合元素的平均值

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,6 +2,8 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  var sum = collection.reduce((previous, current) => current += previous);
+  return sum / collection.length;
 }
 
 module.exports = compute_average;
