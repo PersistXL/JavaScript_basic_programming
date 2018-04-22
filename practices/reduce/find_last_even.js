3fee5a69b34ff8ed61047d221f37b29f2cb542fd
PersistXL <pointing@gmail.com>
2018-04-22 16:48:26 +0800

    找出给定集合元素的最后一个偶数

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,13 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  for (var i = collection.length-1;i>=0;i--) {
+    if (collection[i] % 2 === 0) {
+      var result = collection[i];
+      break;
+    }
+  }
+  return result;
 }
 
 module.exports = find_last_even;
