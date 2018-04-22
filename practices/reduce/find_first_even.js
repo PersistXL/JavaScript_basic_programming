4db3a3b6d27d3c9424ec0a77f6f0c0759cbfb5a7
PersistXL <pointing@gmail.com>
2018-04-22 16:44:39 +0800

    找出给定集合元素的第一个偶数

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,6 +2,13 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  for (var insex in collection) {
+    if (collection[insex] % 2 === 0) {
+      var result = collection[insex];
+      break;
+    }
+  }
+  return result;
 }
 
 module.exports = find_first_even;
