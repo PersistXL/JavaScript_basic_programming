a33cae47b2295fe8929c29def8e08b7c2b227038
PersistXL <pointing@gmail.com>
2018-04-25 17:09:42 +0800

    数组的平均数（如果是小数上取整）映射为字母

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function average_to_letter(collection) {
-
   //在这里写入代码
+  var sum = 0;
+  var letter = ["", "a", "b","c","d","e","f","g","h","i","j"];
+  for (var indext in collection){
+    sum += collection[indext];
+  }
+  return letter[Math.ceil(sum / collection.length)];
 }
 
 module.exports = average_to_letter;
