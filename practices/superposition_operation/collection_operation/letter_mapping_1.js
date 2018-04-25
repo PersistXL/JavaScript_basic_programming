4111198d6f7289c20b608f1df6ff1e24a889947f
PersistXL <pointing@gmail.com>
2018-04-25 16:45:18 +0800

    数组的每个偶数映射为字母

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -3,6 +3,14 @@
 function even_to_letter(collection) {
 
   //在这里写入代码
+  var reslut = [];
+  var letter = ["", "a", "b","c","d","e","f","g","h","i","j"];
+  for (var index in collection) {
+    if (collection[index] % 2 == 0) {
+      reslut.push(letter[collection[index]]);
+    }
+  }
+  return reslut;
 }
 
 module.exports = even_to_letter;
