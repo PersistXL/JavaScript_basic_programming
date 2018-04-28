ed5db2bb5da6665edcef6b4bedaaeb1a688ef366
PersistXL <pointinglxl@gmail.com>
2018-04-28 20:39:55 +0800

    '1-10的奇数算平均数'

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,14 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+  var result = [];
+  for (var i = 0 ; i<collection.length;i++) {
+    if (collection[i] % 2 !== 0) {
+      result.push(collection[i]);
+    }
+  }
+  var max = eval(result.join("+"));
+  return max / result.length;
 }
 
 module.exports = average_uneven;
