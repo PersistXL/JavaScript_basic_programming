723a830e32fb0be7c0cc70465b44e369db5ec1fd
PersistXL <pointinglxl@gmail.com>
2018-04-28 20:32:12 +0800

    '每一个奇数*3+5再求总和'

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -3,6 +3,14 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var result = [];
+  for (var i =0 ;i<collection.length;i++) {
+    if (collection[i] % 2 !== 0) {
+      result.push(collection[i]);
+    }
+  }
+  result = result.map(result => result * 3 + 5);
+  return eval(result.join("+"));
 }
 
 module.exports = hybrid_operation_to_uneven;
