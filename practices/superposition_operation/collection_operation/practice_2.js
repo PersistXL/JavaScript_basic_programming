414dd104ec2f634247258c22b1a8442738ffc105
PersistXL <pointinglxl@gmail.com>
2018-04-28 20:27:51 +0800

    '每一个奇数*3+2'

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -3,6 +3,14 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var result = [];
+  for (var i = 0; i<collection.length;i++) {
+    if (collection[i] % 2 !== 0 ) {
+      result.push(collection[i]);
+    }
+
+  }
+  return result.map(collection => (collection * 3 + 2));
 }
 
 module.exports = hybrid_operation_to_uneven;
