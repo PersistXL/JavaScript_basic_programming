a4d3284fba5a5a3ac64c04273a554d2e990634d3
PersistXL <pointinglxl@gmail.com>
2018-04-28 20:06:52 +0800

    '每一个数*3+2再算总和'

--- practices/superposition_operation/collection_operation/practice_1.js
+++ practices/superposition_operation/collection_operation/practice_1.js
@@ -3,6 +3,8 @@
 function hybrid_operation(collection) {
 
   //在这里写入代码
+  var result = collection.map(collection => (collection * 3 + 2));
+  return eval(result.join("+"));
 }
 
 module.exports = hybrid_operation;
