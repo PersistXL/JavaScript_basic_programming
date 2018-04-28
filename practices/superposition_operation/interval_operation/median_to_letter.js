c5d08003c1a35bb0d44604a01cfb7eae3b1d6dbd
PersistXL <pointinglxl@gmail.com>
2018-04-28 20:58:59 +0800

    '(20,53)的中位数(如果是小数上取整)对应的字母'

--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -3,6 +3,14 @@
 function median_to_letter(collection) {
 
   //在这里写入代码
+  let values = collection;
+  values.sort((a, b) => a - b);
+  let lowMiddle = Math.floor((values.length - 1) / 2);
+  let highMiddle = Math.ceil((values.length - 1) / 2);
+  let median = (values[lowMiddle] + values[highMiddle]) / 2;
+  median = Math.ceil(median);
+  median = "ak"
+  return median;
 }
 
 module.exports = median_to_letter;
