0c79e46bba0499eec2778c4ed140c8fd341a4f99
PersistXL <pointinglxl@gmail.com>
2018-04-28 21:07:47 +0800

    '计算第偶数个元素的平均数'

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,14 @@
 'use strict';
 var calculate_average = function(collection){
-
+  //在这里输入代码
+  var sum = 0;
+  var count = 0
+  for (var index in collection) {
+    if (collection[index] % 2  === 0) {
+      sum += collection[index];
+      count++;
+    }
+  }
+  return sum / count;
 };
 module.exports = calculate_average;
