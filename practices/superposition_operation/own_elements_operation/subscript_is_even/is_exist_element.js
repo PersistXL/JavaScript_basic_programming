89406a685012e29c3567497351e049d6ecf5545a
PersistXL <pointinglxl@gmail.com>
2018-04-28 21:22:37 +0800

    '判断元素的偶数个是否存在'

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,15 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  var result = [];
+  for (var index in collection) {
+    if (collection[index] % 2 == 0) {
+      result.push(collection[index]);
+    }
+  }
+  if (result.length >= element) {
+    return true;
+  }else {
+    return false
+  }
 };
 module.exports = is_exist_element;
