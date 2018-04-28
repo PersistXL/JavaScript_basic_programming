f9f1c1790705b4b81a60bdec42c25ca1aa111c42
PersistXL <pointinglxl@gmail.com>
2018-04-28 21:24:53 +0800

    'single_element'

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,36 @@
 'use strict';
 var single_element = function(collection){
+  var result = [];
+  var resu = [];
+  for (var i = 0; i < collection.length; i++) {
+    if ((i + 1) % 2 === 0) {
+      result.push(collection[i]);
+    }
+  }
 
+  for (var i = 0; i < distinct(result).length - 1; i++) {
+    resu.push(distinct(result)[i]);
+  }
+
+  if (distinct(result).length !== 0) {
+    return resu;
+  }else {
+    return [];
+  }
+
+
+};
+function distinct(arr) {
+  var ret = [],
+    length = arr.length;
+  for (var i = 0; i < length; i++) {
+    for (var j = i + 1; j < length; j++) {
+      if (arr[i] === arr[j]) {
+        j = ++i;
+      }
+    }
+    ret.push(arr[i + 1]);
+  }
+  return ret;
 };
 module.exports = single_element;
