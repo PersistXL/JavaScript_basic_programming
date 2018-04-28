8f4ed90aa01e6bf4a1fdc6fa64e3319f01ab39d0
PersistXL <pointinglxl@gmail.com>
2018-04-28 21:40:56 +0800

    '每一个数与下一个数相加，乘以3,最后一个数不算'

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,5 +1,10 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-  return [12,24,36,48,60,72,84,96,108,120];
+  var result = [];
+  for (var i=0;i<collection.length;i++) {
+    result.push((collection[i] + collection[i + 1]) * 3);
+
+  }
+  return result.splice(0,result.length-1);
 }
 module.exports = one_add_next_multiply_three;
