beaf9150b38cf5cfee7e9f5998901a8fde662887
PersistXL <pointinglxl@gmail.com>
2018-04-28 20:35:40 +0800

    '1-10的偶数算总数'

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -3,6 +3,13 @@
 function amount_even(collection) {
 
   //在这里写入代码
+  var result = [];
+  for (var i = 0; i<collection.length;i++) {
+    if (collection[i] % 2 ===0) {
+      result.push(collection[i]);
+    }
+  }
+  return eval(result.join("+"));
 }
 
 module.exports = amount_even;
