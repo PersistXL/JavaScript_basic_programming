321cbc4c7638982e5fcb6e04bd2271d556ab7c55
PersistXL <pointinglxl@gmail.com>
2018-04-28 21:34:52 +0800

    '偶数奇数分两头，偶数部分从小到大，奇数部分从大到小'

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,22 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
+  var oushu = [];
+  var jishu = [];
+  for (var i in collection) {
+    if (collection[i] % 2 === 0) {
+      oushu.push(collection[i]);
+    }else {
+      jishu.push(collection[i]);
+    }
+  }
 
+  oushu.sort(function (x,y) {
+    return x-y;
+  });
+
+  jishu.sort(function (x,y) {
+    return y - x;
+  })
+  return oushu.concat(jishu)
 };
 module.exports = even_asc_odd_desc;
